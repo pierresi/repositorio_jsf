759dae289a3ca056b3334c64a19e469940401dc5
Pierre Martin <Pierre Martin@Pierre>
2013-03-24 17:05:48 -0300

    Adição das entidades relacionadas

---
+++ repositorio_jsf/src/main/java/com/mycompany/repositorio_jsf/model/etities/Endereco.java
@@ -0,0 +1,33 @@
+package com.mycompany.repositorio_jsf.model.etities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="endereco")
+
+public class Endereco implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    @Column (name="IdEndereco", nullable=false)
+    private Integer idEndereco;
+    @Column (name="Bairro", length=80)
+    private String bairro;
+    @Column (name="NomeLogradouro", length=80)
+    private String nomeLogradouro;
+    @Column (name="CEP", length=80)
+    private String cep;
+    @Column (name="Numero")
+    private Integer numero;
+    @Column (name="Complemento", length=80)
+    private String complemente;
+    
+    
+}
