6dd72c4bdc32eb2d655d077a3657e411d349939a
Pierre Martin <Pierre Martin@Pierre>
2013-03-29 14:34:20 -0300

    Criação do faces-config.xml e do PhaseListenerRepositorio

--- repositorio_jsf/src/main/java/com/mycompany/repositorio_jsf/util/FacesContextUtil.java
+++ repositorio_jsf/src/main/java/com/mycompany/repositorio_jsf/util/FacesContextUtil.java
@@ -4,13 +4,14 @@ import javax.faces.context.FacesContext;
 import org.hibernate.Session;
 
 public class FacesContextUtil {
-    
+
     private static final String HIBERNATE_SESSION = "hibernate_session";
 
     public static void setRequestSession(Session session){
         FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE_SESSION, session);
     }
-    public static Session getHIBERNATE_SESSION() {
+    
+    public static Session getRequestSession() {
         return (Session)FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get(HIBERNATE_SESSION);
     }
     
